2020f97bc44586a68b32f541370a4116ddf88fd3
Ticiana Milfont <ticionamilfont@gmail.com>
2026-09-14 17:00:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
@@ -19,10 +20,12 @@
         }     
         ?>
 
+<div class="container">
+
         <h1> Nome: <?=$nome?> </h1>
         <h1> Idade: <?=$idade?> </h1>
         <p> <?=$nome?> é <?=$resposta?> </p>
-
+    </div>
         
 </body>
 </html>
