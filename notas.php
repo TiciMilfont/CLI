097a0413d913105e2b0d3f5f2054da6bd91dd057
Ticiana Milfont <ticionamilfont@gmail.com>
2026-09-21 15:27:11 -0300

    primeira versão do calculo de notas

--- notas.php
+++ notas.php
@@ -2,6 +2,7 @@
 <?php
 $nome = "nome";
         $idade = "idade";
+        $soma_da_nota = "soma_da_nota"
         $soma_dos_pesos = "soma_dos_pesos";
         $media = "media";
         $situacao = "situacao";
@@ -10,6 +11,12 @@ $nome = "nome";
         $nota3 = "nota3";
         $nota4 = "nota4";
         $nota5 = "nota5";
+        $peso1 = 2;
+        $peso2 = 3;
+        $peso3 = 1;
+        $peso4 = 1;
+        $peso5 = 3;
+        $resposta = "resposta";
 
          /*$_SERVER  variável pré-definida do PHP */
 
@@ -22,13 +29,24 @@ $nome = "nome";
             $nota3 = $_POST ["nota3"];
             $nota4 = $_POST ["nota4"];
             $nota5 = $_POST ["nota5"];
+            $soma_da_nota = $_POST ["soma_da_nota"];
+            $soma_dos_pesos = $_POST ["soma_dos_pesos"];
+            $media = $_POST ["media"];
+
+            $soma_da_nota =  ($nota1 * $peso1) + ($nota2 * $peso2) + ($nota3 * $peso3) + ($nota4 * $peso4) + ($nota5 * $peso5);
+            $soma_dos_pesos = $peso1 + $peso2 + $peso3 + $peso4 + $peso5 = 10;
+            $media = $soma_da_nota / $soma_dos_pesos;
 
 
-        if ($media <= 18) {
-            $resposta = "é menor de idade.";
+        if ($media >= 7) {
+            $situacao = "APROVADO";
+        }
+
+        if else ($media >= 5 && <= 7) {
+            $situacao = "RECUPERAÇÃO";
         }
         else {
-            $resposta = "é maior de idade.";
+            $resposta = "REPROVADO";
         }   }  
 
         ?>
@@ -60,8 +78,25 @@ $nome = "nome";
     </div>
 
     <div>      
-        <input type="number" id="ano" name="ano">
+        <input type="number" id="nota1" name="nota1">
     </div>
+
+    <div>      
+        <input type="number" id="nota2" name="nota2">
+    </div>
+
+    <div>      
+        <input type="number" id="nota3" name="nota3">
+    </div>
+
+    <div>      
+        <input type="number" id="nota4" name="nota4">
+    </div>
+
+    <div>      
+        <input type="number" id="nota5" name="nota5">
+    </div>
+
 
     <button type="submit">Enviar</button>
 
@@ -70,12 +105,12 @@ $nome = "nome";
 
 </div>
   
-<?php if ($resposta != "")  { ?>
+<?php if ($situacao != "")  { ?>
 
     <p> Nome: <?=$nome ?></p>
     <p> Idade: <?=$idade ?></p>
-    <p> Ano de Nascimento: <?=$ano ?></p>
-    <h1> <?=$nome?> <?=$resposta ?></h1>
+    <p> Média: <?=$media ?></p>
+    <h1> <?=$nome?> <?=$situacao ?></h1>
     
     <?php } ?>
 
